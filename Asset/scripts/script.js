ce52647cc3ca83591be12bdcd6581a6f7af845c6
Nihanth Sripathi <nihanthsripathi@gmail.com>
2024-10-11 21:20:23 +0530

    added authentication

--- Asset/scripts/script.js
+++ Asset/scripts/script.js
@@ -4,4 +4,20 @@ const loginLink = document.querySelector('.login-link')
 const SecondSignup = document.querySelector('.Second-SignUp')
 registerLink.onclick = () => wrapper.classList.add('active');
 loginLink.onclick = () => wrapper.classList.toggle('active', false);
-SecondSignup.onclick = () => wrapper.classList.toggle('active', false);
+
+
+// making the lable field dissapper ...while using input field
+const inputBoxes = document.querySelectorAll('.input-box');
+
+inputBoxes.forEach(inputBox => {
+    const input = inputBox.querySelector('input');
+    const label = inputBox.querySelector('label');
+
+    input.addEventListener('input', () => {
+        if (input.value !== '') {
+            label.style.display = 'none';
+        } else {
+            label.style.display = 'block';
+        }
+    });
+});
